357435e3915fb5c416cf2292a4dcbffaf6d538ec
bestmaster99 <Tcheajiawei@hotmail.com>
2015-08-08 12:52:07 +0800

    Add count_vowels method to count vowels

--- index.rb
+++ index.rb
@@ -1,8 +1,3 @@
-
-#initalize vowel_array
-vowel_array =['a','e','i','o','u']
-
-
 #read filename
 def read_file_as_string(filename)
   data = ''
@@ -10,12 +5,33 @@ def read_file_as_string(filename)
   f.each_line do |line|
     data += line
   end
-    clean_text(data)
+    return data
 end 
 
 #strip whitespaces,lowercase and remove punctuation
 def clean_text(datacontent)
-  puts datacontent.gsub(/[^a-z0-9]/i, "").downcase
+  return datacontent.gsub(/[^a-z0-9]/i, "").downcase
 end
 
-  read_file_as_string 'ch5.txt'
+#Search and count vowels in textfile
+def count_vowels(datacontent)
+  #initalize vowel_array
+  vowel_list ={'a'=>0, 'e'=>0, 'i'=>0, 'o'=> 0, 'u'=>0}
+  datacontent.each_char do |i|   
+    vowel_list.each_key do |a|
+      if i == a
+        vowel_list[i] += 1 
+      #  puts i
+      end
+     #puts a
+    end
+  end
+    puts vowel_list['u']
+end
+
+
+
+  textfile = read_file_as_string 'ch5.txt'
+  newtextfile = clean_text(textfile)
+  #puts newtextfile
+  count_vowels(newtextfile)
