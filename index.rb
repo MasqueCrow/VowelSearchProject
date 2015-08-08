65fdc21719cc03d70c59c4fc9e0276842cec2658
bestmaster99 <Tcheajiawei@hotmail.com>
2015-08-08 11:44:54 +0800

    Add clean_file method to remove spaces and punctuation and lowercase text

--- index.rb
+++ index.rb
@@ -1,10 +1,21 @@
+
+#initalize vowel_array
+vowel_array =['a','e','i','o','u']
+
+
+#read filename
 def read_file_as_string(filename)
   data = ''
   f = File.open(filename,"r")
   f.each_line do |line|
     data += line
   end
-    puts data
+    clean_text(data)
 end 
+
+#strip whitespaces,lowercase and remove punctuation
+def clean_text(datacontent)
+  puts datacontent.gsub(/[^a-z0-9]/i, "").downcase
+end
 
   read_file_as_string 'ch5.txt'
